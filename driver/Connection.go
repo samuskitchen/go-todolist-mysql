a35e0787f861184c50e541e9a8f2fb4f8a9ad77f
Daniel De La Pava Suarez <daniel.samkit@gmail.com>
2020-02-17 14:27:15 -0500

    add base todo list app

---
+++ driver/Connection.go
@@ -0,0 +1,38 @@
+package driver
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+)
+
+type DB struct {
+	SQL *gorm.DB
+}
+
+var dbConn = &DB{}
+
+//var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
+
+func ConnectSQL(host, port, user, pass, dbname string) (*DB, error) {
+
+	dbSource := fmt.Sprintf(
+		"%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		user,
+		pass,
+		dbname,
+	)
+
+	log.Println(dbSource)
+
+	db, err := gorm.Open("mysql", dbSource)
+	if err != nil {
+		log.Println("error connecting to the database: ", err)
+		panic(err)
+	}
+
+	dbConn.SQL = db
+	return dbConn, err
+}
--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	
+	"fmt"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+	"os"
+)
+
 func main() {
-	$END$
+
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPort := os.Getenv("DB_PORT")
+
+	connection, err := driver.ConnectSQL(dbHost, dbPort, dbUser, "", dbName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	defer connection.SQL.Close()
+
+
+	log.Info("Starting TodoList API server")
+	router := mux.NewRouter()
+	router.HandleFunc("/healthz", HealThz).Methods("GET")
+	http.ListenAndServe(":8080", router)
 }
+
+func HealThz(w http.ResponseWriter, r *http.Request) {
+	log.Info("API Health is OK")
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"alive": true}`)
+}
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetReportCaller(true)
+}
+
